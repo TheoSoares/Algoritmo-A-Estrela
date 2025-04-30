e03388dc6bca467a55d38cf11193c49b99e4d1a7
TheoSoares <theocsoares@gmail.com>
2025-04-30 20:18:55 -0300

    gerador de imagens aprimorado

--- gerar_imagem_com_mapa/gerador_de_imagem.py
+++ gerar_imagem_com_mapa/gerador_de_imagem.py
@@ -2,24 +2,19 @@ from PIL import Image
 import numpy as np
 
 def gerar_imagem(caminho_arquivo):
-    # Abre o arquivo e lê a matriz
     with open(caminho_arquivo, 'r') as f:
         linhas = f.readlines()
     
-    # Prepara uma lista de listas com os valores da matriz
     matriz = []
     for linha in linhas:
         matriz.append([char for char in linha.strip()])
     
-    # Converte a matriz para um array numpy
     altura = len(matriz)
     largura = len(matriz[0])
     
-    # Cria uma imagem com o tamanho da matriz
     imagem = Image.new('RGB', (largura, altura))
     pixels = imagem.load()
     
-    # Mapeamento de cores para os valores
     cores = {
         '0': (255, 255, 255),  # Branco
         '1': (0, 0, 0),        # Preto
@@ -31,17 +26,14 @@ def gerar_imagem(caminho_arquivo):
         '^': (0, 0, 255)       # Azul (para caminho)
     }
     
-    # Preenche a imagem com os valores da matriz
     for y in range(altura):
         for x in range(largura):
             valor = matriz[y][x]
             if valor in cores:
                 pixels[x, y] = cores[valor]
     
-    # Salva a imagem gerada como PNG
-    imagem.save('matriz_imagem.png')
+    imagem.save(caminho_arquivo[:-4] + '_imagem.png')
     print("Imagem gerada com sucesso: matriz_imagem.png")
 
-# Caminho do arquivo com a matriz (substitua pelo caminho correto)
 caminho_arquivo = input()
 gerar_imagem(caminho_arquivo)
